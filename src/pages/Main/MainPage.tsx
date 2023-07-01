02c0f06a186694031c96e9f8f12b5248e90025e1
Evgeny Trizno <zenatrizno9@gmail.com>
2023-07-01 12:50:41 +0300

    updt

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -16,7 +16,7 @@ export const MainPage: FC = () => {
                     </Text>
                     <Slider container="100%">
                         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-                            <Card checkbox={false} />
+                            <Card checkbox={false} key={item} />
                         ))}
                     </Slider>
                 </div>
