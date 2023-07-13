8533dbd7eef2d6057b0d9013524c118704f716c1
Evgeny Trizno <zenatrizno9@gmail.com>
2023-07-13 23:46:33 +0300

    updt

--- src/pages/MobileCreateEvent/MobileCreateEventPage.tsx
+++ src/pages/MobileCreateEvent/MobileCreateEventPage.tsx
@@ -10,6 +10,8 @@ import {
 import { BackArrow, Btn, MobileFilter, Switch, Text } from "@/shared";
 
 import info from "/assets/info-circle.svg";
+import sortArrow from "/assets/sort-arrow.svg";
+import file from "/assets/file.svg";
 import styles from "./MobileCreateEventPage.module.scss";
 
 const MobileCreateEventPage: FC = () => {
@@ -18,7 +20,85 @@ const MobileCreateEventPage: FC = () => {
             id: useId(),
             title: "Требуется ли переводчик",
             subtitle: "Выберите предпочитаемый язык",
-            content: <div></div>,
+            content: (
+                <div className={styles.lang}>
+                    <div className={`${styles.langBox} ${styles.grayBlock}`}>
+                        <Text type="p" fz="12px">
+                            Русский
+                        </Text>
+                    </div>
+                    <div className={styles.sort}>
+                        <img src={sortArrow} alt="" />
+                    </div>
+                    <div className={`${styles.langBox} ${styles.grayBlock}`}>
+                        <Text type="p" fz="12px">
+                            Арабский
+                        </Text>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            id: useId(),
+            title: "Уведомить",
+            subtitle: "Выберите время для уведомления",
+            content: (
+                <div className={styles.time}>
+                    <Text type="p" fz="14px">
+                        За
+                    </Text>
+                    <div className={`${styles.grayBlock} ${styles.timeBlock}`}>
+                        <Text type="p" fz="12px">
+                            20
+                        </Text>
+                    </div>
+                    <Text type="p" fz="14px">
+                        минут
+                    </Text>
+                </div>
+            ),
+        },
+        {
+            id: useId(),
+            title: "Добавить аккаунт",
+            subtitle: "Выберите время для уведомления",
+            content: (
+                <div className={styles.accounts}>
+                    <div className={styles.list}>
+                        <div className={styles.account}>
+                            <MobileCard />
+                        </div>
+                    </div>
+                    <div>
+                        <Text
+                            type="h4"
+                            fz="13px"
+                            color="#7D7F82"
+                            position="end"
+                        >
+                            Добавить +
+                        </Text>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            id: useId(),
+            title: "Прикрепить файл",
+            subtitle: "Выберите материал",
+            content: (
+                <div className={styles.files}>
+                    <div className={styles.select}>
+                        <div className={styles.file}></div>
+                    </div>
+                    <div className={styles.filesBtn}>
+                        <img src={file} alt="" />
+                        <Text type="h2" color="#7D7F82" fz="13px">
+                            Добавить файл
+                        </Text>
+                    </div>
+                </div>
+            ),
         },
     ];
 
@@ -69,38 +149,27 @@ const MobileCreateEventPage: FC = () => {
                         </Text>
                         <div className={styles.items}>
                             {data.map((item) => (
-                                <div className={styles.item}>
-                                    <Text type="p" fz="15px">
-                                        {item.title}
-                                    </Text>
-                                    <Switch />
+                                <div className={styles.item} key={item.id}>
+                                    <div className={styles.block}>
+                                        <Text type="p" fz="15px">
+                                            {item.title}
+                                        </Text>
+                                        <Switch />
+                                    </div>
                                     <div className={styles.content}>
                                         <div className={styles.inner}>
-                                            <Text type="p" color="#7D7F82">
+                                            <Text
+                                                type="p"
+                                                color="#7D7F82"
+                                                fz="12px"
+                                            >
                                                 {item.subtitle}
                                             </Text>
+                                            {item.content}
                                         </div>
                                     </div>
                                 </div>
                             ))}
-                            <div className={styles.item}>
-                                <Text type="p" fz="15px">
-                                    Уведомить
-                                </Text>
-                                <Switch />
-                            </div>
-                            <div className={styles.item}>
-                                <Text type="p" fz="15px">
-                                    Добавить аккаунт
-                                </Text>
-                                <Switch />
-                            </div>
-                            <div className={styles.item}>
-                                <Text type="p" fz="15px">
-                                    Прикрепить файл
-                                </Text>
-                                <Switch />
-                            </div>
                             <div className={styles.item}>
                                 <Text type="p" fz="15px">
                                     Дополнительный опрос специалиста
