da677169c69005a5c5e8bbccc0b3eeff2a399402
Evgeny <zenatrizno9@gmail.com>
2023-09-08 11:21:35 +0300

    added type for btn

--- src/widgets/components/ExtraCallMobile/ExtraCallMobile.tsx
+++ src/widgets/components/ExtraCallMobile/ExtraCallMobile.tsx
@@ -1,6 +1,7 @@
-import { FC, useId, MouseEvent } from "react";
+import { FC, useId, MouseEvent, useState } from "react";
 import { IExtraCallBtnData } from "./types";
 import { motion } from "framer-motion";
+import { TABLET } from "@/shared/utils";
 
 import { useExtraCall, useUserCondition } from "@/shared/model/store";
 
@@ -8,15 +9,15 @@ import amabulance from "/assets/amabulance-blue.svg";
 import people from "/assets/people-blue.svg";
 import marker from "/assets/marker-plus.svg";
 import styles from "./ExtraCallMobile.module.scss";
+import { Modal } from "../Modal/Modal";
 
 export const ExtraCallMobile: FC = () => {
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(true);
+
     const { condition } = useUserCondition();
     const { isOpen, setIsOpen } = useExtraCall();
 
     const sick = condition === "Болен";
-    const media = window.matchMedia(
-        "(min-width: 576px) and (max-width: 768px)"
-    ).matches;
 
     const btns: IExtraCallBtnData[] = [
         {
@@ -26,9 +27,10 @@ export const ExtraCallMobile: FC = () => {
             },
             id: useId(),
             position: {
-                x: media ? -90 : -70,
-                y: media ? 30 : 50,
+                x: TABLET ? -90 : -70,
+                y: TABLET ? 30 : 50,
             },
+            type: "скорая помощь",
         },
         {
             icon: {
@@ -38,16 +40,18 @@ export const ExtraCallMobile: FC = () => {
             id: useId(),
             position: {
                 x: 0,
-                y: media ? -20 : 0,
+                y: TABLET ? -20 : 0,
             },
+            type: "",
         },
         {
             icon: { healthy: people, sick: people },
             id: useId(),
             position: {
-                x: media ? 90 : 70,
-                y: media ? 30 : 50,
+                x: TABLET ? 90 : 70,
+                y: TABLET ? 30 : 50,
             },
+            type: "поддержка",
         },
     ];
 
@@ -59,53 +63,71 @@ export const ExtraCallMobile: FC = () => {
         setIsOpen(false);
     };
 
+    const handleClickOnBtn = (e: MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+
+        // const target = e.target as HTMLDivElement;
+        // const type = target.getAttribute("data-type");
+
+        // switch (type) {
+        //     case "скорая помощь":
+        //         setIsOpenModal(true);
+        //         break;
+        // }
+    };
+
     return (
-        <motion.div
-            onClick={handleClick}
-            style={{
-                pointerEvents: !isOpen ? "none" : "auto",
-                overflow: "hidden",
-            }}
-            className={styles.extra}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: isOpen ? 1 : 0 }}
-            transition={{ duration: 0.3 }}
-            exit={{ opacity: 0 }}
-        >
-            <div
-                className={styles.block}
+        <>
+            <motion.div
+                onClick={handleClick}
                 style={{
-                    bottom: "10%",
+                    pointerEvents: !isOpen ? "none" : "auto",
+                    overflow: "hidden",
                 }}
+                className={styles.extra}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isOpen ? 1 : 0 }}
+                transition={{ duration: 0.3 }}
+                exit={{ opacity: 0 }}
             >
-                {btns.map((btn) => (
-                    <motion.div
-                        className={styles.btn}
-                        onClick={(e: MouseEvent<HTMLDivElement>) =>
-                            e.stopPropagation()
-                        }
-                        initial={{ x: 0, y: 0 }}
-                        whileTap={{ scale: 0.9 }}
-                        animate={{
-                            x: isOpen ? btn.position.x : 0,
-                            y: isOpen ? btn.position.y : 110,
-                        }}
-                        transition={{
-                            type: "spring",
-                            duration: 0.3,
-                            damping: 13,
-                            stiffness: 100,
-                        }}
-                        exit={{ x: -0, y: -0 }}
-                        key={btn.id}
-                    >
-                        <img
-                            src={sick ? btn.icon.sick : btn.icon.healthy}
-                            alt=""
-                        />
-                    </motion.div>
-                ))}
-            </div>
-        </motion.div>
+                <div
+                    className={styles.block}
+                    style={{
+                        bottom: "10%",
+                    }}
+                >
+                    {!isOpenModal &&
+                        btns.map((btn) => (
+                            <motion.div
+                                className={styles.btn}
+                                onClick={handleClickOnBtn}
+                                initial={{ x: 0, y: 0 }}
+                                whileTap={{ scale: 0.9 }}
+                                animate={{
+                                    x: isOpen ? btn.position.x : 0,
+                                    y: isOpen ? btn.position.y : 110,
+                                }}
+                                transition={{
+                                    type: "spring",
+                                    duration: 0.3,
+                                    damping: 13,
+                                    stiffness: 100,
+                                }}
+                                exit={{ x: -0, y: -0 }}
+                                key={btn.id}
+                                data-type={btn.type}
+                            >
+                                <img
+                                    src={
+                                        sick ? btn.icon.sick : btn.icon.healthy
+                                    }
+                                    alt=""
+                                />
+                            </motion.div>
+                        ))}
+                </div>
+            </motion.div>
+            {/* {isOpenModal && <Modal setIsOpenModal={setIsOpenModal}></Modal>} */}
+        </>
     );
 };
