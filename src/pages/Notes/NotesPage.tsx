8ceae45e6680330a7bd886d22f07d4737b62065b
Evgeny <zenatrizno9@gmail.com>
2023-09-14 23:01:44 +0300

    page changed

--- src/pages/Notes/NotesPage.tsx
+++ src/pages/Notes/NotesPage.tsx
@@ -1,42 +1,70 @@
 import { FC } from "react";
 import { useNavigate } from "react-router";
 
-import { Calendar, Search, NoteBlock } from "@/widgets";
 import { Layout } from "../Layout/Layout";
-import { Btn, Text } from "@/shared";
+import { Filter, Text, Btn } from "@/shared";
+import { NoteBlock, Search } from "@/widgets";
 
+import ghost from "/assets/ghost.svg";
 import addCircle from "/assets/add-circle.svg";
 import styles from "./NotesPage.module.scss";
 
 const NotesPage: FC = () => {
+    const data = [1, 2, 3, 4];
     const navigate = useNavigate();
 
     return (
         <Layout>
-            <div className={styles.container}>
+            <div className={styles.notes}>
                 <div className={styles.header}>
                     <Search placeholder="Введите запрос" height="48px" />
                     <Btn
                         color="#0064FA"
-                        width="60px"
+                        width="48px"
+                        height="48px"
                         onClick={() => navigate("/create-event")}
-                        padding="0"
+                        padding="14px"
+                        br="12px"
                     >
                         <img src={addCircle} alt="" />
                     </Btn>
                 </div>
-                <div className={styles.wrapper}>
-                    <Calendar height="max-content" width="100%" info={false} />
-                    <div className={styles.list}>
-                        <Text type="p" color="#7D7F82" fz="14px">
-                            10 Октябрь
-                        </Text>
-                        {[1, 2, 3, 4, 5].map((item) => (
-                            <NoteBlock
-                                onClick={() => navigate(`/notes/${item}`)}
-                            />
-                        ))}
-                    </div>
+                <Filter data={["Текущие", "Доп.проверка"]} />
+                <div
+                    className={styles.items}
+                    style={
+                        data.length === 0
+                            ? { display: "block" }
+                            : { display: "grid" }
+                    }
+                >
+                    {data.length === 0 ? (
+                        <div className={styles.empty}>
+                            <div className={styles.inner}>
+                                <img src={ghost} alt="" />
+                                <Text type="h2" color="#262626" fz="32px">
+                                    Ничего нет
+                                </Text>
+                                <div className={styles.text}>
+                                    <Text
+                                        type="p"
+                                        fz="24px"
+                                        position="center"
+                                        color="#26262680"
+                                    >
+                                        Список пуст, создайте запись нажав на
+                                        кнопку “Плюс”
+                                    </Text>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            {data.map((item) => (
+                                <NoteBlock key={item} />
+                            ))}
+                        </>
+                    )}
                 </div>
             </div>
         </Layout>
