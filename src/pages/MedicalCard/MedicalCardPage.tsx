469c087f8710ebd7b1d0488e73b1cfa8c3f6cfb6
Evgeny <zenatrizno9@gmail.com>
2023-09-19 20:42:21 +0300

    changed name

---
+++ src/pages/MedicalCard/MedicalCardPage.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { useNavigate } from "react-router";
+
+import { Calendar, MedicalBlock } from "@/widgets";
+import { Layout } from "../Layout/Layout";
+import { Text } from "@/shared";
+
+import styles from "./MedicalCardPage.module.scss";
+
+const MedicalCardPage: FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <div className={styles.box}>
+                        <Calendar
+                            height="max-content"
+                            width="100%"
+                            info={false}
+                        />
+                    </div>
+                    <div className={styles.list}>
+                        <Text type="p" color="#7D7F82" fz="14px">
+                            10 Октябрь
+                        </Text>
+                        {[1, 2, 3, 4, 5].map((item) => (
+                            <MedicalBlock
+                                onClick={() => navigate(`/notes/${item}`)}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default MedicalCardPage;
