01f63a8b1370195daefcd1914a0210b6b8bc1688
Evgeny <zenatrizno9@gmail.com>
2023-10-05 23:02:02 +0300

    indocator component

---
+++ src/widgets/components/Indicator/Indicator.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+import { Text } from "@/shared";
+
+import arrow from "/assets/arrow-top-green.svg";
+import arrowRed from "/assets/arrow-top-red.svg";
+import styles from "./Indicator.module.scss";
+
+interface IIndicatorProps {
+    type: "up" | "low";
+    value: string;
+}
+
+export const Indicator: FC<IIndicatorProps> = ({ value, type }) => {
+    return (
+        <div className={styles.indicator}>
+            <img src={type === "up" ? arrow : arrowRed} alt="" />
+            <Text type="h4" fz="12px" color="#00CC5E">
+                {value}
+            </Text>
+        </div>
+    );
+};
