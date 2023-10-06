353977179f491ce08821a23360b5269969943602
Evgeny <zenatrizno9@gmail.com>
2023-10-06 23:45:52 +0300

    adaptive

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 
 import { Chat, Header } from "@/widgets";
+import { SMALL_LAPTOP, PC } from "@/shared/utils";
 
 import styles from "./ChatPage.module.scss";
 
 const ChatPage: FC = () => {
     return (
         <div className={styles.chat}>
-            <Header width="100%" />
+            {(PC || SMALL_LAPTOP) && <Header width="100%" />}
             <div className={styles.view}>
                 <Chat />
             </div>
