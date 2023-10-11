395c755cc83ba09390f3ae28cd9aa383cf3ee755
Evgeny <zenatrizno9@gmail.com>
2023-10-12 01:04:31 +0300

    vercel fixed

--- src/pages/Notes/NotesPage.tsx
+++ src/pages/Notes/NotesPage.tsx
@@ -25,7 +25,7 @@ const NotesPage: FC = () => {
     return (
         <Layout>
             {MOBILE && (
-                <CustomMobileHeader>
+                <CustomMobileHeader back>
                     <BackArrow />
                 </CustomMobileHeader>
             )}
