5ca50429a18b5765200fcb8c6bbc631743ec6a39
Evgeny <zenatrizno9@gmail.com>
2023-10-18 00:00:09 +0300

    updt with date

--- src/widgets/components/MailHeader/MailHeader.tsx
+++ src/widgets/components/MailHeader/MailHeader.tsx
@@ -1,7 +1,35 @@
 import { FC } from "react";
 
+import { Text } from "@/shared";
+import { SMALL_LAPTOP } from "@/shared/utils";
+
 import styles from "./MailHeader.module.scss";
 
-export const MailHeader: FC = () => {
-    return <div className={styles.mailHeader}></div>;
+export interface IMailHeaderProps {
+    date: boolean;
+}
+
+export const MailHeader: FC<IMailHeaderProps> = ({ date = true }) => {
+    return (
+        <div
+            className={styles.mailHeader}
+            style={{
+                gridTemplateColumns:
+                    !date || SMALL_LAPTOP ? "125px 1fr 125px" : "",
+            }}
+        >
+            <Text type="h4" fz={SMALL_LAPTOP ? "12px" : "14px"} color="#7D7F82">
+                От
+            </Text>
+            <Text type="h4" fz={SMALL_LAPTOP ? "12px" : "14px"} color="#7D7F82">
+                Сообщение
+            </Text>
+            {!SMALL_LAPTOP ||
+                (date && (
+                    <Text type="h4" fz="14px" color="#7D7F82" position="center">
+                        Дата
+                    </Text>
+                ))}
+        </div>
+    );
 };
