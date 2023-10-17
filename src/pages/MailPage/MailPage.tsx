1580402ab697aed9677b6cdea9e6f69cd560e1fe
Evgeny <zenatrizno9@gmail.com>
2023-10-17 23:58:23 +0300

    fixed bugs

--- src/pages/MailPage/MailPage.tsx
+++ src/pages/MailPage/MailPage.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
 
 import { Layout } from "../Layout/Layout";
-import { AdminPanelContainer, MailHeader, MailLayout } from "@/widgets";
+import {
+    AdminPanelContainer,
+    MailHeader,
+    MailLayout,
+    MailMessage,
+} from "@/widgets";
 import { Filter, Text } from "@/shared";
 
 import styles from "./MailPage.module.scss";
+import { SMALL_LAPTOP } from "@/shared/utils";
 
 const MailPage: FC = () => {
     return (
@@ -21,11 +27,34 @@ const MailPage: FC = () => {
                         ]}
                     />
                 </div>
-                <Text type="p" position="center" color="#7D7F82">
+                <Text
+                    type="p"
+                    position="center"
+                    fz={SMALL_LAPTOP ? "14px" : ""}
+                    color="#7D7F82"
+                >
                     Сегодня
                 </Text>
                 <MailLayout>
-                    <MailHeader />
+                    <MailHeader date />
+                    <div className={styles.list}>
+                        {[1, 2, 3, 4].map((item) => (
+                            <MailMessage date key={item} />
+                        ))}
+                    </div>
+                </MailLayout>
+                <Text
+                    type="p"
+                    position="center"
+                    fz={SMALL_LAPTOP ? "14px" : ""}
+                    color="#7D7F82"
+                >
+                    Полученные ранее
+                </Text>
+                <MailLayout>
+                    {[1, 2, 3, 4].map((item) => (
+                        <MailMessage date key={item} />
+                    ))}
                 </MailLayout>
             </AdminPanelContainer>
         </Layout>
