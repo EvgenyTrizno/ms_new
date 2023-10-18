04f209b8e35cb5a03c247886ac1539e0ba9daf74
Evgeny <zenatrizno9@gmail.com>
2023-10-18 23:52:37 +0300

    added income components

--- src/widgets/components/IncomeBlock/IncomeBlock.tsx
+++ src/widgets/components/IncomeBlock/IncomeBlock.tsx
@@ -1,5 +1,54 @@
 import { FC } from "react";
 
-export const IncomeBlock: FC = () => {
-    return <div>IncomeBlock</div>;
+import { BlueBox } from "../BlueBox/BlueBox";
+import { Text } from "@/shared";
+import { LAPTOP } from "@/shared/utils";
+import { WhiteContentBlock } from "../WhiteContentBlock/WhiteContentBlock";
+
+import styles from "./IncomeBlock.module.scss";
+
+interface IData {
+    title: string;
+    sum: number;
+}
+
+interface IIncomeBlockProps {
+    title: string;
+    data: IData[];
+}
+
+export const IncomeBlock: FC<IIncomeBlockProps> = ({ title, data }) => {
+    return (
+        <WhiteContentBlock>
+            <Text type="h2" fz={LAPTOP ? "17px" : "18px"}>
+                {title}
+            </Text>
+            <div className={styles.bal}>
+                {data.map((item, i) => (
+                    <div className={styles.sum} key={i}>
+                        <Text type="p" fz={LAPTOP ? "14px" : ""}>
+                            {item.title}
+                        </Text>
+                        <BlueBox
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                width: "100%",
+                                padding: "12px 16px",
+                                gridGap: 10,
+                                marginTop: 5,
+                            }}
+                        >
+                            <Text type="h2" fz={LAPTOP ? "26px" : "28px"}>
+                                $
+                            </Text>
+                            <Text type="p" fz="20px">
+                                {item.sum.toLocaleString("en-US")}
+                            </Text>
+                        </BlueBox>
+                    </div>
+                ))}
+            </div>
+        </WhiteContentBlock>
+    );
 };
