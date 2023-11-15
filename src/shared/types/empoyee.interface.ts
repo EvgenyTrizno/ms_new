6bbcfe756169e46cb265dbd9b403890dcb3ce89d
Evgeny <zenatrizno9@gmail.com>
2023-11-15 23:29:15 +0300

    added types

---
+++ src/shared/types/empoyee.interface.ts
@@ -0,0 +1,25 @@
+export interface IEmpoyee {
+    id: number;
+    password: string;
+    last_login: null | Date;
+    first_name: string;
+    last_name: string;
+    number: string;
+    email: null | string;
+    middle_name: string;
+    address: string;
+    specialization: string;
+    work_experience: string;
+    registration_date: Date;
+    review_date: null | Date;
+    review_passed: null;
+    created_at: Date;
+    updated_at: Date;
+    verification_code: number;
+    reset_code: number;
+    email_verification_code: number;
+    city: number;
+    country: number;
+    center: number;
+    clinic: null | number;
+}
