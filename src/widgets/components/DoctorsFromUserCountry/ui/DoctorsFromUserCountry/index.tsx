082522c650b2650e47b58bf00c1b68eacaac82ec
Evgeny <zenatrizno9@gmail.com>
2023-11-15 23:26:38 +0300

    doctors from user country

---
+++ src/widgets/components/DoctorsFromUserCountry/ui/DoctorsFromUserCountry/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+import { Rows } from "@/shared/ui/Rows";
+import { Row } from "@/shared/ui/Row";
+import { Text } from "@/shared/ui/Text";
+import { SliderArrows } from "@/widgets";
+import { List } from "../List";
+import { useAuth } from "@/shared/model/store/auth";
+
+export const DoctorsFromUserCountry: FC = () => {
+    const { user } = useAuth();
+
+    return (
+        <Rows gap={10} rows={["auto"]}>
+            <Row gap={0} style={{ justifyContent: "space-between" }}>
+                <Text type="p" color="#7D7F82" fz="14px">
+                    {user && user.country
+                        ? `Специалисты из ${user.country.name}`
+                        : "Неудалось определить страну"}
+                </Text>
+                <SliderArrows />
+            </Row>
+            <List />
+        </Rows>
+    );
+};
