c8e0c2cd23324155859937bc185cc367f32ed4e4
Andrey <teryaev.2004@mail.ru>
2024-01-24 17:01:21 +0300

    Fixed chat template

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -1,20 +1,41 @@
 import { FC } from "react";
-import { Layout } from "../Layout";
 import { MOBILE } from "@/shared/utils";
 import { Header } from "./ui/Header/Header";
-import styles from "./index.module.scss";
+import styles from "./styles.module.scss";
 
 const ChatPage: FC = () => {
   return (
-    <>
+    <div>
       {MOBILE && <Header />}
 
-      <Layout>
-        <div className={styles.chatWrapper}>
-          
+      <div className={styles.chatWrapper}>
+        <div className={styles.requirement}>
+          Требуется доп. врач для выполнения задания
         </div>
-      </Layout>
-    </>
+
+        <div className={styles.messagesList}>
+          <p>1</p>
+
+        </div>
+
+        <button className={styles.medHelp}>
+          <img src="/assets/icons/med-help.svg" alt="med-help" />
+          <span>Мед. помощь</span>
+        </button>
+      </div>
+
+      <div className={styles.footer}>
+        <img src="/assets/icons/attach.svg" alt="attach" />
+
+        <div className={styles.inputWrapper}>
+          <input type="text" placeholder="Введите сообщение..." />
+
+          <img src="/assets/icons/smile.svg" alt="smile" />
+        </div>
+
+        <img src="/assets/icons/micro.svg" alt="micro" />
+      </div>
+    </div>
   );
 };
 
