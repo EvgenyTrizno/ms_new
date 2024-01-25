05b2d354d950e1b37c726fcc901f06e4d7e5b60a
Andrey <teryaev.2004@mail.ru>
2024-01-25 18:47:53 +0300

    Fixed Filter component

--- src/shared/ui/Filter/index.tsx
+++ src/shared/ui/Filter/index.tsx
@@ -6,43 +6,40 @@ import { useAuth } from "@/shared/model/store/auth";
 import styles from "./styles.module.scss";
 
 export const Filter: FC<IFilter> = ({
-    style,
-    data,
-    width,
-    icons,
-    setIsSelect,
-    isSelect,
+  data,
+  icons,
+  setIsSelect,
+  isSelect,
 }) => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-    const active = `${styles.item} ${styles.active}`;
-    const activeRed = `${styles.item} ${styles.activeRed}`;
+  const active = `${styles.item} ${styles.active}`;
+  const activeRed = `${styles.item} ${styles.activeRed}`;
 
-    const sick = user && user.disease.length;
+  const sick = user && user.disease.length;
 
-    return (
+  return (
+    <div
+      className={styles.container}
+    >
+      {data.map((item, i) => (
         <div
-            className={styles.container}
-            style={{ ...style, width, borderColor: sick ? "#F7E6E8" : "" }}
+          className={`${
+            isSelect === item && sick
+              ? activeRed
+              : isSelect === item && !sick
+              ? active
+              : styles.item
+          }`}
+          key={item}
+          onClick={() => {
+            setIsSelect(item);
+          }}
         >
-            {data.map((item, i) => (
-                <div
-                    className={`${
-                        isSelect === item && sick
-                            ? activeRed
-                            : isSelect === item && !sick
-                            ? active
-                            : styles.item
-                    }`}
-                    key={item}
-                    onClick={() => {
-                        setIsSelect(item);
-                    }}
-                >
-                    <span>{item}</span>
-                    {item && icons && icons[i]}
-                </div>
-            ))}
+          <span>{item}</span>
+          {item && icons && icons[i]}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
