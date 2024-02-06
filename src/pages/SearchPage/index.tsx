1260296089d60fcfb4575a6f8d13b001e940050f
Andrey <teryaev.2004@mail.ru>
2024-02-06 20:43:30 +0300

    Added loader for notes data in NotesList component

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -8,6 +8,7 @@ import { FilterBtn } from "@/shared/ui/FilterBtn";
 import { Container } from "@/shared/ui/Container";
 import { Search } from "@/features/Search";
 import { WithFilter } from "./ui/WithFilter";
+import styles from "./styles.module.scss";
 
 const SearchPage: FC = () => {
   const [search, setSearch] = useState("");
@@ -23,7 +24,7 @@ const SearchPage: FC = () => {
 
       <Layout>
         <Container>
-          <div>
+          <div className={styles.content}>
             <Search
               placeholder="Поиск чатов"
               height="48px"
