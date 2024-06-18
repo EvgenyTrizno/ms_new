1342e972cff1869fe37813680ab4153e71de11a5
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-06-18 18:03:48 +0300

    login

--- src/shared/config/axiosInstance.ts
+++ src/shared/config/axiosInstance.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
-  headers: {
-    "Access-Control-Allow-Origin": import.meta.env.VITE_API_URL,
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
+    // baseURL: import.meta.env.VITE_API_URL + "/api",
+    baseURL: "http://83.147.246.220:8000/api",
+    headers: {
+        "Access-Control-Allow-Origin": import.meta.env.VITE_API_URL,
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
 });
